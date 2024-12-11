9f62b83492816547b1bef87a5f28b61f0c799256
Matti Tahvonen <matti@vaadin.com>
2024-12-11 19:58:43 +0200

    disabled FilmText (obsoleted in mysql version, not existing in postgres)

--- src/main/java/com/example/demo/entities/FilmText.java
+++ src/main/java/com/example/demo/entities/FilmText.java
@@ -8,7 +8,17 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-@Entity
+/**
+ * TODO figure out if this should be removed altogether, old obsoleted mysql hack. So not really
+ * needed for MySQL version either...
+ * Quote from https://dev.mysql.com/doc/sakila/en/sakila-structure-tables-film-text.html:
+ * 5.1.10 The film_text Table
+ *
+ * The film_text table contains the film_id, title and description columns of the film table, with the contents of the table kept in synchrony with the film table by means of triggers on film table INSERT, UPDATE and DELETE operations (see Section 5.5, “Triggers”).
+ *
+ * Before MySQL server 5.6.10, the film_text table was the only table in the Sakila sample database that used the MyISAM storage engine. This is because full-text search is used for titles and descriptions of films listed in the film table. MyISAM was used because full-text search support with InnoDB was not available until MySQL server 5.6.10.
+ */
+//@Entity
 @Table(name = "film_text")
 public class FilmText {
     @Id
