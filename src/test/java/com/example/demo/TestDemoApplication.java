083bf680b1d13e87ce9a0b602f6e8f93d6de5384
Matti Tahvonen <matti@vaadin.com>
2024-12-12 10:07:31 +0200

    don't ask 🤫

--- src/test/java/com/example/demo/TestDemoApplication.java
+++ src/test/java/com/example/demo/TestDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 public class TestDemoApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("vaadin.vaadin.frontend.frontend.folder", "./target/frontend");
 		SpringApplication.from(DemoApplication::main).with(TestcontainersConfiguration.class).run(args);
 	}
 
