b9fa8aaf37aaa4a983df5ebb854cc14bce33c876
Matti Tahvonen <matti@vaadin.com>
2024-12-17 23:33:37 +0200

    Use in.virit.entityexplorer:entity-explorer dependency instead of local UI classes
    Faster and more features
    
    Tackling (when merged to main branches) #1, #2, #3

--- src/test/java/com/example/demo/entityexplorerui/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package contains the viever classess for some entities,
- * that can be used when running the TestDemoApplication during development.
- * These will not be there in production.
- */
-package com.example.demo.entityexplorerui;
