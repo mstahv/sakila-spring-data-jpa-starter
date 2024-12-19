5ac23e9f9ae47e86285ca0980275424e97822e0a
Matti Tahvonen <matti@vaadin.com>
2024-12-19 09:07:55 +0200

    interface instead of impl in interface

--- src/main/java/com/example/demo/vaadinui/SpringDataGrid.java
+++ src/main/java/com/example/demo/vaadinui/SpringDataGrid.java
@@ -3,13 +3,14 @@ package com.example.demo.vaadinui;
 import com.vaadin.flow.function.SerializableFunction;
 import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.vaadin.firitin.components.grid.VGrid;
 
 import java.util.stream.Stream;
 
 public class SpringDataGrid<T> extends VGrid<T> {
 
-    interface LazyDataBinding<T> extends SerializableFunction<PageRequest, Stream<T>> {
+    interface LazyDataBinding<T> extends SerializableFunction<Pageable, Stream<T>> {
 
     }
 
