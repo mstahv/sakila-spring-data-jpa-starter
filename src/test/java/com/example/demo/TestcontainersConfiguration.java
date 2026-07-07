97e13b1249740b7eb96e799d3670aa10061c72ab
Matti Tahvonen <matti@vaadin.com>
2026-07-07 12:36:30 +0300

    SB 4.1.0 and Vaadin 25.2

--- src/test/java/com/example/demo/TestcontainersConfiguration.java
+++ src/test/java/com/example/demo/TestcontainersConfiguration.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
